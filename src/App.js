abea5b65bb63cb756c879298d935fb830ac873ea
Neeraj Dattu Dudam <dudamdattu@gmail.com>
2023-11-30 22:19:38 +0530

    checking ternary operations

--- src/App.js
+++ src/App.js
@@ -1,33 +1,19 @@
 import './App.css';
 
-const User = (props) => {
-  return (
-    <div>
-      <h1>{props.name}</h1>
-      <h2>{props.age}</h2>
-      <h3>{props.email}</h3>
-    </div>
-  )
-}
-
-const Job = (props) => {
-  return (
-    <div>
-      <h1>{props.salary}</h1>
-      <h2>{props.position}</h2>
-      <h3>{props.company}</h3>
-    </div>
-  )
-}
-
 
 const App = () => {
-  
+
+  const isGreen = false;
+
   return (
-    <div className="App">      
-      <User name='Neeraj' age={22} email='neeraj@gmail.com' />
-      <Job salary={2345} position='senior' company='amazon'/>
-      
+    <div className="App">
+
+      {isGreen ?
+        <h1 style={{ color: isGreen ? "red" : "green" }} >Green</h1>
+        : 
+        <h1 style={{ color: isGreen ? "red" : "green" }}>Red</h1>
+      }
+
     </div>
   );
 }
