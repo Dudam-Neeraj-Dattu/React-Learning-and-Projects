9c74ff6a8841b75be6a3fc36d0a73c102989a9bf
Neeraj Dattu Dudam <dudamdattu@gmail.com>
2023-12-01 20:20:39 +0530

    checking pre usestate usage

--- src/App.js
+++ src/App.js
@@ -1,35 +1,26 @@
 import './App.css';
 
-const User = (props) => {
-  return (
-    <div>
-      <h1>{props.name}</h1>
-      <h2>{props.age}</h2>
-      <h3>{props.email}</h3>
-    </div>
-  )
-}
-
-const Job = (props) => {
-  return (
-    <div>
-      <h1>{props.salary}</h1>
-      <h2>{props.position}</h2>
-      <h3>{props.company}</h3>
-    </div>
-  )
-}
-
-
 const App = () => {
-  
-  return (
-    <div className="App">      
-      <User name='Neeraj' age={22} email='neeraj@gmail.com' />
-      <Job salary={2345} position='senior' company='amazon'/>
-      
-    </div>
-  );
+
+    var num = 0;
+
+    const increase = () => {
+        num += 1;
+        console.log(num);
+    }
+
+    const decrease = () => {
+        num -=1 ;
+        console.log(num);
+    }
+
+    return (
+        <div className="App">
+            <button onClick={decrease}>Decrease num</button>
+            <h1>{num}</h1>
+            <button onClick={increase}>Increase num</button>
+        </div>
+    );
 }
 
 export default App;
