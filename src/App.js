8a0ec991b23ed45c03b36d8a6cd7e070f1abd2fe
Neeraj Dattu Dudam <dudamdattu@gmail.com>
2023-12-01 17:12:27 +0530

    components

--- src/App.js
+++ src/App.js
@@ -1,41 +1,50 @@
 import './App.css';
+import { User } from './components/User.js';
+// import { User } from './Components/User';
+
 
 
 const App = () => {
 
-  // const isGreen = false;
-  const users = [
-    {
-      name: "Neeraj",
-      age: 21,
-      email: "neeraj@gmail.com"
-    },
-    {
-      name: "jagan",
-      age: 19,
-      email: "jagan@gmail.com"
-    },
-    {
-      name: "shiva",
-      age: 51,
-      email: "shiva@gmail.com"
-    }
-  ]
+    const users = [
+        {
+            name: "Neeraj",
+            age: 21,
+            email: "neeraj@gmail.com"
+        },
+        {
+            name: "jagan",
+            age: 19,
+            email: "jagan@gmail.com"
+        },
+        {
+            name: "shiva",
+            age: 51,
+            email: "shiva@gmail.com"
+        }
+    ]
 
 
-  return (
-    <div className="App">
-      {users.map((user, key) => {
-        return (
-          <div>
-            <h1 key={key}>{user.name}</h1>
-            <h2 key={key}>{user.age}</h2>
-            <h3 key={key}>{user.email}</h3>
-          </div>
-        )
-      })}
-    </div>
-  );
+    return (
+        <div className="App">
+            {users.map((user, key) => {
+                return (
+                    <div>
+                        <h1 key={key}>{user.name}</h1>
+                        <h2 key={key}>{user.age}</h2>
+                        <h3 key={key}>{user.email}</h3>
+                    </div>
+                )
+            })}
+
+            {users.map((user, key) => {
+                return (
+                    <User key={key} name={user.name} age={user.age} email={user.email} />
+                )
+
+            })}
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/User.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const User = (props) => {
+    return (
+        <div>
+            <h1>{props.name}</h1>
+            <h2>{props.age}</h2>
+            <h3>{props.email}</h3>
+        </div>
+    )
+}
+
+// export default User
