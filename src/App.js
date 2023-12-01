897d13203d1535c4a6bacb6cb062bc02887975b0
Neeraj Dattu Dudam <dudamdattu@gmail.com>
2023-12-01 16:58:21 +0530

    using map

--- src/App.js
+++ src/App.js
@@ -3,17 +3,37 @@ import './App.css';
 
 const App = () => {
 
-  const isGreen = false;
+  // const isGreen = false;
+  const users = [
+    {
+      name: "Neeraj",
+      age: 21,
+      email: "neeraj@gmail.com"
+    },
+    {
+      name: "jagan",
+      age: 19,
+      email: "jagan@gmail.com"
+    },
+    {
+      name: "shiva",
+      age: 51,
+      email: "shiva@gmail.com"
+    }
+  ]
+
 
   return (
     <div className="App">
-
-      {isGreen ?
-        <h1 style={{ color: isGreen ? "red" : "green" }} >Green</h1>
-        : 
-        <h1 style={{ color: isGreen ? "red" : "green" }}>Red</h1>
-      }
-
+      {users.map((user, key) => {
+        return (
+          <div>
+            <h1 key={key}>{user.name}</h1>
+            <h2 key={key}>{user.age}</h2>
+            <h3 key={key}>{user.email}</h3>
+          </div>
+        )
+      })}
     </div>
   );
 }
