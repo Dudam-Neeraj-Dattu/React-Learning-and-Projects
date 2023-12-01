1c6b02e66abd11bab636eb944f9f1396fe50598a
Neeraj Dattu Dudam <dudamdattu@gmail.com>
2023-12-01 20:53:41 +0530

    added change color feature using usestate hook

--- src/App.js
+++ src/App.js
@@ -3,16 +3,35 @@ import { useState } from 'react'
 
 const App = () => {
 
-    const [text, setText] = useState('');
+    const [text, setText] = useState('I am Neeraj Dattu');
+    const [display, setDisplay] = useState(false);
+    const [color, setColor] = useState('rgb(0,0,0)')
 
-    const updatetext = (event) => {
-        setText(event.target.value);
+    const updatetext = () => {
+        if(display === true) {
+            setText('I am Neeraj Dattu');
+            setDisplay(false);
+        }
+        else {
+            setText('');
+            setDisplay(true);
+        }
+    }
+
+    const changecolor = () => {
+        var min = 0, max = 255;
+        const r = min + (Math.random() * (max - min));
+        const g = min + (Math.random() * (max - min));
+        const b = min + (Math.random() * (max - min));
+        console.log(r, g, b);        
+        setColor('rgb(' + r + ',' + g + ',' + b);
     }
 
     return (
         <div className="App">
-            <input type='text' onChange={updatetext} />
-            <h1>{text}</h1>
+            <button onClick={updatetext}>Show/Hide</button>
+            <h1 style={{color: color}}>{text}</h1>
+            <button onClick={changecolor}>change color</button>
         </div>
     );
 }
