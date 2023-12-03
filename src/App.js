6e88fdfdd264112e76ab71073d7a91895d3ad9f5
Neeraj Dattu Dudam <dudamdattu@gmail.com>
2023-12-03 15:37:10 +0530

    excercise

--- src/App.js
+++ src/App.js
@@ -1,29 +1,80 @@
 import './App.css';
 import { useState } from 'react'
+import { Task } from './Task';
 
 const App = () => {
 
-    const [count, setCount] = useState(0)
-    
-    const increase = () => {
-        setCount(count + 1)
+    const [task, setTask] = useState('')
+    const [list, setList] = useState([]);    
+
+    const taskText = (event) => {
+        setTask(event.target.value)
     }
 
-    const decrease = () => {
-        setCount(count - 1)
+    const updateList = () => {
+        if (task.length > 0) {
+            const updatedList = list;
+            updatedList.push(
+                {
+                    id: list.length === 0 ? 1 : list[list.length - 1].id + 1,
+                    taskName: task,
+                    completed: false,
+                }
+            );
+            setList(updatedList);            
+            document.getElementById('text').value = '';
+            setTask('');
+        }
+        else {
+            alert('enter the input');
+        }
     }
 
-    const setzero = () => {
-        setCount(0);
+    const deleteList = (key) => {        
+        setList(list.filter((name) => {
+            return name.id !== key
+        }));
     }
 
+    const complete = (id) => {
+
+        setList(
+            list.map((task) => {
+                if (task.id === id) {
+                    return {                    
+                        ...task,
+                        completed: true
+                    }
+                }
+                else{
+                    return task;
+                }
+            })
+        ) 
+
+    }
 
     return (
         <div className="App">
-            <button onClick={increase}>increase</button>
-            <button onClick={decrease}>decrease</button>
-            <button onClick={setzero}>set to zero</button>
-            <h1>{count}</h1>
+            <div className="addTask">
+                <input type="text" id='text' onChange={taskText} />
+                <br />
+                <button id='addtask' onClick={updateList}>Add Task</button>
+            </div>
+            <div className="showTask">
+                {list.map((taskname) => {
+                    return (
+                        <Task
+                            taskName={taskname.taskName}
+                            deleteList={deleteList}
+                            complete={complete}
+                            id={taskname.id}
+                            completed={taskname.completed}
+                        />
+                    )
+                })}
+
+            </div>
         </div>
     );
 }
